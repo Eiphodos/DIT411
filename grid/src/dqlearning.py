ffbbd8f6392e92fe0dd1977ed33cfb3f4a2a5812
Eiphodos <david.hagerman.olzon@gmail.com>
2019-03-05 14:58:54 +0100

    Cooperative networks
    Changed the single neural network to three different stand-alone networks and updated the code for the wolfs/sheeps a bit.

--- grid/src/dqlearning.py
+++ grid/src/dqlearning.py
@@ -29,14 +29,20 @@ def main(mode):
         if not os.path.exists('trained_model/'):
             os.mkdir('trained_model/')
 
-        model = NeuralNetwork()
+        model1 = NeuralNetwork()
+        model2 = NeuralNetwork()
+        model3 = NeuralNetwork()
 
         if cuda_available:
-            model = model.cuda()
+            model1 = model1.cuda()
+            model2 = model2.cuda()
+            model3 = model3.cuda()
 
-        model.apply(init_weights)
+        model1.apply(init_weights)
+        model2.apply(init_weights)
+        model3.apply(init_weights)
         start = time.time()
-        train(model, start)
+        train_networks(model1, model2, model3 , start)
 
 
 def test(model):
@@ -82,7 +88,233 @@ def test(model):
         # set state to be state_1
         state = state_1
 
-def train(model, start):
+def train_networks(model1, model2, model3, start):
+
+    # define Adam optimizer
+    optimizer1 = optim.Adam(model1.parameters(), model1.learn_rate)
+    optimizer2 = optim.Adam(model2.parameters(), model2.learn_rate)
+    optimizer3 = optim.Adam(model3.parameters(), model3.learn_rate)
+
+    # initialize replay memory
+    replay_memory1 = []
+    replay_memory2 = []
+    replay_memory3 = []
+
+    #Cuda
+    cuda_available = torch.cuda.is_available()
+
+    # initialize epsilon value
+    epsilon1 = model1.init_epsilon
+    epsilon_decrements1 = np.linspace(model1.init_epsilon, model1.fin_epsilon, model1.iterations)
+    epsilon2 = model2.init_epsilon
+    epsilon_decrements2 = np.linspace(model2.init_epsilon, model2.fin_epsilon, model2.iterations)
+    epsilon3 = model3.init_epsilon
+    epsilon_decrements3 = np.linspace(model3.init_epsilon, model3.fin_epsilon, model3.iterations)
+
+    # initialize mean squared error loss
+    criterion = nn.MSELoss()
+
+     # instantiate game
+    game_state = State(5, 1, 1)
+
+    action1 = torch.zeros([model1.n_actions], dtype=torch.float32)
+    action2 = torch.zeros([model2.n_actions], dtype=torch.float32)
+    action3 = torch.zeros([model3.n_actions], dtype=torch.float32)
+    # Set initial action to 'do nothing' for all three wolves
+    action1[0] = 1
+    action2[0] = 1
+    action3[0] = 1
+
+    #Get game grid and reward
+    grid, reward1, reward2, reward3, finished = game_state.frame_step(action1, action2, action3)
+
+    #Convert to tensor
+    tensor_data = torch.Tensor(grid)
+
+    if cuda_available:
+        tensor_data = tensor_data.cuda()
+    # Concatenate four last grids
+    state = tensor_data.unsqueeze(0).unsqueeze(0)
+
+    # Initialize iterion counter for this epoch
+    iteration = 0
+
+    while iteration < model1.iterations:
+
+        # get output from the neural network
+        output1 = model1(state)[0]
+        output2 = model2(state)[0]
+        output3 = model3(state)[0]
+
+        # initialize actions
+        action1 = torch.zeros([model1.n_actions], dtype=torch.float32)
+        action2 = torch.zeros([model2.n_actions], dtype=torch.float32)
+        action3 = torch.zeros([model3.n_actions], dtype=torch.float32)
+        if cuda_available:  # put on GPU if CUDA is available
+            action1 = action1.cuda()
+            action2 = action2.cuda()
+            action3 = action3.cuda()
+        # epsilon greedy exploration wolf 1
+        random_action1 = random.random() <= epsilon1
+        action_index_1 = [torch.randint(model1.n_actions, torch.Size([]), dtype=torch.int)
+                        if random_action1
+                        else torch.argmax(output1)][0]
+
+        # epsilon greedy exploration wolf 2
+        random_action2 = random.random() <= epsilon2
+        action_index_2 = [torch.randint(model2.n_actions, torch.Size([]), dtype=torch.int)
+                        if random_action2
+                        else torch.argmax(output2)][0]
+
+        # epsilon greedy exploration wolf 3
+        random_action3 = random.random() <= epsilon3
+        action_index_3 = [torch.randint(model3.n_actions, torch.Size([]), dtype=torch.int)
+                        if random_action3
+                        else torch.argmax(output3)][0]
+
+
+        if cuda_available:  # put on GPU if CUDA is available
+            action_index_1 = action_index_1.cuda()
+            action_index_2 = action_index_2.cuda()
+            action_index_3 = action_index_3.cuda()
+
+
+        action1[action_index_1] = 1
+        action2[action_index_2] = 1
+        action3[action_index_3] = 1
+
+        # State
+        grid, reward1, reward2, reward3, finished = game_state.frame_step(action1, action2, action3)
+        tensor_data_1 = torch.Tensor(grid)
+        if cuda_available:
+            tensor_data_1 = tensor_data_1.cuda()
+        state_1 = tensor_data_1.unsqueeze(0).unsqueeze(0)
+
+        action1 = action1.unsqueeze(0)
+        action2 = action2.unsqueeze(0)
+        action3 = action3.unsqueeze(0)
+        reward1 = torch.from_numpy(np.array([reward1], dtype=np.float32)).unsqueeze(0)
+        reward2 = torch.from_numpy(np.array([reward2], dtype=np.float32)).unsqueeze(0)
+        reward3 = torch.from_numpy(np.array([reward3], dtype=np.float32)).unsqueeze(0)
+
+        # save transition to replay memory
+        replay_memory1.append((state, action1, reward1, state_1, finished))
+        replay_memory2.append((state, action2, reward2, state_1, finished))
+        replay_memory3.append((state, action3, reward3, state_1, finished))
+
+        # if replay memory is full, remove the oldest transition
+        if len(replay_memory1) > model1.memory_size:
+            replay_memory1.pop(0)
+        if len(replay_memory2) > model2.memory_size:
+            replay_memory2.pop(0)
+        if len(replay_memory3) > model3.memory_size:
+            replay_memory3.pop(0)
+
+
+        # epsilon annealing
+        epsilon1 = epsilon_decrements1[iteration]
+        epsilon2 = epsilon_decrements2[iteration]
+        epsilon3 = epsilon_decrements3[iteration]
+
+        # sample random minibatch
+        minibatch1 = random.sample(replay_memory1, min(len(replay_memory1), model1.minibatch_size))
+        minibatch2 = random.sample(replay_memory2, min(len(replay_memory2), model2.minibatch_size))
+        minibatch3 = random.sample(replay_memory3, min(len(replay_memory3), model3.minibatch_size))
+
+
+        # unpack minibatch 1
+        state_batch1 = torch.cat(tuple(d[0] for d in minibatch1))
+        action_batch1 = torch.cat(tuple(d[1] for d in minibatch1))
+        reward_batch1 = torch.cat(tuple(d[2] for d in minibatch1))
+        state_1_batch1 = torch.cat(tuple(d[3] for d in minibatch1))
+
+        # unpack minibatch 2
+        state_batch2 = torch.cat(tuple(d[0] for d in minibatch2))
+        action_batch2 = torch.cat(tuple(d[1] for d in minibatch2))
+        reward_batch2 = torch.cat(tuple(d[2] for d in minibatch2))
+        state_1_batch2 = torch.cat(tuple(d[3] for d in minibatch2))
+
+        # unpack minibatch 3
+        state_batch3 = torch.cat(tuple(d[0] for d in minibatch3))
+        action_batch3 = torch.cat(tuple(d[1] for d in minibatch3))
+        reward_batch3 = torch.cat(tuple(d[2] for d in minibatch3))
+        state_1_batch3 = torch.cat(tuple(d[3] for d in minibatch3))
+
+        if cuda_available:  # put on GPU if CUDA is available
+            state_batch1 = state_batch1.cuda()
+            state_batch2 = state_batch2.cuda()
+            state_batch3 = state_batch3.cuda()
+            action_batch1 = action_batch1.cuda()
+            action_batch2 = action_batch2.cuda()
+            action_batch3 = action_batch3.cuda()
+            reward_batch1 = reward_batch1.cuda()
+            reward_batch2 = reward_batch2.cuda()
+            reward_batch3 = reward_batch3.cuda()
+            state_1_batch1 = state_1_batch1.cuda()
+            state_1_batch2 = state_1_batch2.cuda()
+            state_1_batch3 = state_1_batch3.cuda()
+
+        # get output for the next state
+        output_1_batch1 = model1(state_1_batch1)
+        output_1_batch2 = model2(state_1_batch2)
+        output_1_batch3 = model3(state_1_batch3)
+
+        # set y_j to r_j for terminal state, otherwise to r_j + gamma*max(Q)
+        y_batch_1 = torch.cat(tuple(reward_batch1[i] if minibatch1[i][4]
+                            else reward_batch1[i] + model1.gamma * torch.max(output_1_batch1[i])
+                            for i in range(len(minibatch1))))
+        y_batch_2 = torch.cat(tuple(reward_batch2[i] if minibatch2[i][4]
+                            else reward_batch2[i] + model2.gamma * torch.max(output_1_batch2[i])
+                            for i in range(len(minibatch2))))
+        y_batch_3 = torch.cat(tuple(reward_batch3[i] if minibatch3[i][4]
+                            else reward_batch3[i] + model3.gamma * torch.max(output_1_batch3[i])
+                            for i in range(len(minibatch3))))
+
+        # extract Q-value
+        q_value_1 = torch.sum(model1(state_batch1) * action_batch1, dim=1)
+        q_value_2 = torch.sum(model2(state_batch2) * action_batch2, dim=1)
+        q_value_3 = torch.sum(model3(state_batch3) * action_batch3, dim=1)
+
+        # PyTorch accumulates gradients by default, so they need to be reset in each pass
+        optimizer1.zero_grad()
+        optimizer2.zero_grad()
+        optimizer3.zero_grad()
+
+        # returns a new Tensor, detached from the current graph, the result will never require gradient
+        y_batch_1 = y_batch_1.detach()
+        y_batch_2 = y_batch_2.detach()
+        y_batch_3 = y_batch_3.detach()
+
+        # calculate loss
+        loss1 = criterion(q_value_1, y_batch_1)
+        loss2 = criterion(q_value_2, y_batch_2)
+        loss3 = criterion(q_value_3, y_batch_3)
+
+        # do backward pass
+        loss1.backward()
+        loss2.backward()
+        loss3.backward()
+        optimizer1.step()
+        optimizer2.step()
+        optimizer3.step()
+
+        # set state to be state_1
+        state = state_1
+        iteration += 1
+
+        if iteration % 25000 == 0:
+            torch.save(model1, "pretrained_model/current_model1_" + str(iteration) + ".pth")
+            torch.save(model2, "pretrained_model/current_model2_" + str(iteration) + ".pth")
+            torch.save(model3, "pretrained_model/current_model3_" + str(iteration) + ".pth")
+
+        print("iteration:", iteration, "elapsed time:", time.time() - start, "epsilon:", epsilon1, "action:",
+                action_index_1.cpu().detach().numpy(), "reward:", reward1.numpy()[0][0], "Q max:",
+                np.max(output1.cpu().detach().numpy()))
+
+
+
+# Training a single network, not currently used
+def train(model1, model2, model3, start):
     # define Adam optimizer
     optimizer = optim.Adam(model.parameters(), lr=1e-6)
 
@@ -95,7 +327,11 @@ def train(model, start):
 
     # initialize epochs
     epoch = 0
+    tot_iterations = 0
 
+     # initialize epsilon value
+    epsilon = model.init_epsilon
+    epsilon_decrements = np.linspace(model.init_epsilon, model.fin_epsilon, model.iterations)
 
     # main infinite loop
     while epoch < model.epochs:
@@ -104,7 +340,7 @@ def train(model, start):
         criterion = nn.MSELoss()
 
         # instantiate game
-        game_state = State(5, 1, 1)
+        game_state = State(10, 1, 1)
 
         action = torch.zeros([model.n_actions], dtype=torch.float32)
         # Set initial action to 'do nothing' for all three wolves
@@ -121,12 +357,9 @@ def train(model, start):
         # Concatenate four last grids
         state = tensor_data.unsqueeze(0).unsqueeze(0)
 
-        # initialize epsilon value
-        epsilon = model.init_epsilon
+        # Initialize iterion counter for this epoch
         iteration = 0
-
-        epsilon_decrements = np.linspace(model.init_epsilon, model.fin_epsilon, model.iterations)
-        print("Epoch: " + str(epoch))
+        
         while (not finished):
 
             # get output from the neural network
@@ -165,14 +398,14 @@ def train(model, start):
             reward = torch.from_numpy(np.array([reward], dtype=np.float32)).unsqueeze(0)
 
             # save transition to replay memory
-            replay_memory.append((state, action, reward, state_1))
+            replay_memory.append((state, action, reward, state_1, finished))
 
             # if replay memory is full, remove the oldest transition
             if len(replay_memory) > model.memory_size:
                 replay_memory.pop(0)
 
             # epsilon annealing
-            epsilon = epsilon_decrements[iteration]
+            epsilon = epsilon_decrements[tot_iterations]
 
             # sample random minibatch
             minibatch = random.sample(replay_memory, min(len(replay_memory), model.minibatch_size))
@@ -193,8 +426,9 @@ def train(model, start):
             output_1_batch = model(state_1_batch)
 
             # set y_j to r_j for terminal state, otherwise to r_j + gamma*max(Q)
-            y_batch = torch.cat(tuple(reward_batch[i]
-                                      for i in range(len(minibatch))))
+            y_batch = torch.cat(tuple(reward_batch[i] if minibatch[i][4]
+                              else reward_batch[i] + model.gamma * torch.max(output_1_batch[i])
+                              for i in range(len(minibatch))))
 
             # extract Q-value
             q_value = torch.sum(model(state_batch) * action_batch, dim=1)
@@ -215,29 +449,30 @@ def train(model, start):
             # set state to be state_1
             state = state_1
             iteration += 1
+            tot_iterations += 1
 
             if iteration % 25000 == 0:
                 torch.save(model, "trained_model/current_model_" + str(iteration) + ".pth")
 
-            #print("iteration:", iteration, "elapsed time:", time.time() - start, "epsilon:", epsilon, "action:",
-            #      action_index_1.cpu().detach().numpy(), "reward:", reward.numpy()[0][0], "Q max:",
-            #  np.max(output.cpu().detach().numpy()))
+            print("Epoch: " + str(epoch) + " iteration:", iteration, "elapsed time:", time.time() - start, "epsilon:", epsilon, "action:",
+                  action_index_1.cpu().detach().numpy(), "reward:", reward.numpy()[0][0], "Q max:",
+              np.max(output.cpu().detach().numpy()))
         epoch += 1
-        input("Press Enter to continue...")
 
 class NeuralNetwork(nn.Module):
 
     def __init__(self):
         super(NeuralNetwork, self).__init__()
 
-        self.n_actions = 15
+        self.n_actions = 5
         self.gamma = 0.99
         self.fin_epsilon = 0.0001
         self.init_epsilon = 0.5
-        self.iterations = 250000
-        self.memory_size = 10000
+        self.iterations = 300000
+        self.memory_size = 100000
         self.minibatch_size = 32
-        self.epochs = 2
+        self.epochs = 1000
+        self.learn_rate = 1e-6
 
         self.conv1 = nn.Conv2d(1, 8, 4, 3)
         self.relu1 = nn.ReLU(inplace=True)
--- grid/src/game/wsgame.py
+++ grid/src/game/wsgame.py
@@ -3,11 +3,10 @@ import random
 
 class State:
     def __init__(self, gridsize, wspeed, sspeed):
-        #Free position = 0, wolf = 1, sheep = 2
+        #Free position = 0, wolf1 = 1, wolf2 = 2, wolf3 = 3, sheep = 4
         self.grid_size = gridsize
         self.grid = np.zeros((gridsize, gridsize), dtype=np.int)
         self.n_wolves = 3
-        self.finished = False
 
         if (self.grid_size * self.grid_size < 9 ):
             raise ValueError('The grid is too small!')
@@ -15,24 +14,24 @@ class State:
         self.wolf_1_pos = (0, 0)
         self.grid[0,0] = 1
         self.wolf_2_pos = (1, 0)
-        self.grid[1, 0] = 1
+        self.grid[1, 0] = 2
         self.wolf_3_pos = (2, 0)
-        self.grid[2, 0] = 1
+        self.grid[2, 0] = 3
 
 
         #Try to place the sheep at the bottom middle of grid
         if self.grid_size % 2 == 0:
             self.sheep_pos = (int(self.grid_size/2), int(self.grid_size -1))
-            if (self.grid[self.sheep_pos[0],self.sheep_pos[1]] == 1):
+            if (self.grid[self.sheep_pos[0],self.sheep_pos[1]] in (1, 2, 3)):
                 raise ValueError('Wolf already placed there, grid probably too small')
             else:
-                self.grid[self.sheep_pos[0],self.sheep_pos[1]] = 2
+                self.grid[self.sheep_pos[0],self.sheep_pos[1]] = 4
         else:
             self.sheep_pos = (int((self.grid_size+1)/2), int(self.grid_size -1))
-            if (self.grid[self.sheep_pos[0],self.sheep_pos[1]] == 1):
+            if (self.grid[self.sheep_pos[0],self.sheep_pos[1]] in (1, 2, 3)):
                 raise ValueError('Wolf already placed there, grid probably too small')
             else:
-                self.grid[self.sheep_pos[0],self.sheep_pos[1]] = 2
+                self.grid[self.sheep_pos[0],self.sheep_pos[1]] = 4
 
         self.wolf_speed = wspeed
         self.sheep_speed = sspeed
@@ -40,6 +39,12 @@ class State:
 
     def move_wolf(self, wolf, dir):
         reward = 0
+        # Reward (punishment) for going out of bounds
+        reward_oob = -0.1
+        # Reward (punishment) for trying to enter an already occupied position
+        reward_ao = -0.01
+        # Reward for catching the sheep
+        reward_catch = 1
 
         if wolf == 0:
             current_pos = self.wolf_1_pos
@@ -51,62 +56,70 @@ class State:
         
         # If action is do nothing, just return
         if dir == 0:
-            return 0
+            return reward
         # Action - move west
         if dir == 1:
-            # If we dont go out of bounds and the new position is not another wolf, we can move the wolf
+            # Check for out of bounds
             if current_pos[1] - self.wolf_speed < 0:
+                reward = reward_oob
                 return reward
-            if self.grid[current_pos[0],current_pos[1] - self.wolf_speed] == 1:
+            # Check if another wolf is already there
+            if self.grid[current_pos[0],current_pos[1] - self.wolf_speed] in (1, 2, 3):
+                reward = reward_ao
                 return reward
             new_pos = (current_pos[0], current_pos[1] - self.wolf_speed)
             if new_pos == self.sheep_pos:
-                reward = 1
-                self.finished = True
+                reward = reward_catch
 
         # Action - move south
         if dir == 2:
             if current_pos[0] + self.wolf_speed >= self.grid_size:
+                reward = reward_oob
                 return reward
-            if self.grid[current_pos[0] + self.wolf_speed,current_pos[1]] == 1:
+            if self.grid[current_pos[0] + self.wolf_speed,current_pos[1]] in (1, 2, 3):
+                reward = reward_ao
                 return reward
             new_pos = (current_pos[0] + self.wolf_speed, current_pos[1])
             if new_pos == self.sheep_pos:
-                reward = 1
-                self.finished = True
+                reward = reward_catch
 
         # Action - move east
         if dir == 3:
             if current_pos[1] + self.wolf_speed >= self.grid_size:
+                reward = reward_oob
                 return reward
-            if self.grid[current_pos[0],current_pos[1] + self.wolf_speed] == 1:
+            if self.grid[current_pos[0],current_pos[1] + self.wolf_speed] in (1, 2, 3):
+                reward = reward_ao
                 return reward
             new_pos = (current_pos[0], current_pos[1] + self.wolf_speed)
             if new_pos == self.sheep_pos:
-                reward = 1
-                self.finished = True
+                reward = reward_catch
 
 
         # Action - move north
         if dir == 4:
             if current_pos[0] - self.wolf_speed < 0:
+                reward = reward_oob
                 return reward
-            if self.grid[current_pos[0] - self.wolf_speed,current_pos[1]] == 1:
+            if self.grid[current_pos[0] - self.wolf_speed,current_pos[1]] in (1, 2, 3):
+                reward = reward_ao
                 return reward
             new_pos = (current_pos[0] - self.wolf_speed, current_pos[1])
             if new_pos == self.sheep_pos:
-                reward = 1
-                self.finished = True
+                reward = reward_catch
+
         # Update positions
-        #Set a wolf at the new position
-        self.grid[new_pos[0],new_pos[1]] = 1
         #Set the old position as free
         self.grid[current_pos[0],current_pos[1]] = 0
+        #Set a wolf at the new position
         if wolf == 0:
+            self.grid[new_pos[0],new_pos[1]] = 1
             self.wolf_1_pos = new_pos
         if wolf == 1:
+            self.grid[new_pos[0],new_pos[1]] = 2
             self.wolf_2_pos = new_pos
         if wolf == 2:
+            self.grid[new_pos[0],new_pos[1]] = 3
             self.wolf_3_pos = new_pos
         return reward
 
@@ -202,13 +215,6 @@ class State:
                 # Flee north as second to last prio
                 direction_preference[2] = 4
 
-        print(self.grid)
-        print(direction_preference)
-        print("Sheep: " + str(self.sheep_pos))
-        print("Closest wolf: " + str(closest_wolf_pos))
-        print(self.wolf_1_pos)
-        print(self.wolf_2_pos)
-        print(self.wolf_3_pos)
 
         # Set sheeps new position to the highest priority one
         successful_escape = False
@@ -230,45 +236,60 @@ class State:
 
         # Update positions
         # Set a sheep at the new position
-        self.grid[new_pos[0],new_pos[1]] = 2
+        self.grid[new_pos[0],new_pos[1]] = 4
         # Set the old position as free
         self.grid[current_pos[0],current_pos[1]] = 0
         self.sheep_pos = new_pos
 
 
-    def frame_step(self, input_actions_tensor):
-        reward = 0
+    def frame_step(self, tensor_action1, tensor_action2, tensor_action3):
+        reward1 = 0
+        reward2 = 0
+        reward3 = 0
 
-        #Move all wolves
+        #Move wolves
         # Actions meanings
-        # action[0] == 1 -  wolf 1 do nothing
-        # action[1] == 1 -  wolf 1  move north
-        # action[2] == 1 -  wolf 1  move east
-        # action[3] == 1 -  wolf 1  move south
-        # action[4] == 1 -  wolf 1  move west
-        # action[5] == 1 -  wolf 2 do nothing
-        # etc etc up to
-        # action[14] == 1 - wolf 3 move west
-        input_actions = input_actions_tensor.cpu().numpy().astype(int)
+        # action[0] == 1 -  wolf do nothing
+        # action[1] == 1 -  wolf move north
+        # action[2] == 1 -  wolf move east
+        # action[3] == 1 -  wolf move south
+        # action[4] == 1 -  wolf move west
+        tensor_action1 = tensor_action1.cpu().numpy().astype(int)
+        tensor_action2 = tensor_action2.cpu().numpy().astype(int)
+        tensor_action3 = tensor_action3.cpu().numpy().astype(int)
 
-        if sum(input_actions) != 1:
+        if sum(tensor_action1) != 1 or sum(tensor_action2) != 1 or sum(tensor_action3) != 1:
             raise ValueError('Not one action per wolf')
 
         wolf = 0
         dir = 0
         counter = 0
-        for action in input_actions:
+        for action in tensor_action1:
             if action == 1:
-                reward = max(reward, State.move_wolf(self, wolf, dir))
+                reward1 = self.move_wolf(0, dir)
             counter += 1
-            if counter % 5 == 0:
-                wolf += 1
             dir = counter % 5
 
-        if (reward == 1):
-            #Sheep was caught so return
-            return self.grid, reward, self.finished
+        for action in tensor_action2:
+            if action == 1:
+                reward2 = self.move_wolf(1, dir)
+            counter += 1
+            dir = counter % 5
+
+        for action in tensor_action3:
+            if action == 1:
+                reward3 = self.move_wolf(2, dir)
+            counter += 1
+            dir = counter % 5
+        print(self.grid)
+        if (reward1 == 1 or reward2 == 1 or reward3 == 1 ):
+            #Sheep was caught so set reward to 1 for all wolves and reset game state
+            reward1 = 1
+            reward2 = 1
+            reward3 = 1
+            self.__init__(self.grid_size, self.wolf_speed, self.sheep_speed)
+            return self.grid, reward1, reward2, reward3, True
         else:
             #Move the sheep
-            State.move_sheep(self)
-            return self.grid, reward, self.finished
+            self.move_sheep()
+            return self.grid, reward1, reward2, reward3, False
